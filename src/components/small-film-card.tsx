09ec970526dc593d0a3b628c7baf0c479e5764ec
Serkik <63666583+Serckik@users.noreply.github.com>
2023-11-05 11:33:42 +0500

    изменения

--- src/components/small-film-card.tsx
+++ src/components/small-film-card.tsx
@@ -6,7 +6,7 @@ export default function SmallFilmCard({id, previewImage, name}: SmallFilmCardPro
   return (
     <>
       <div className="small-film-card__image">
-        <img src={previewImage} alt={name} width="280" height="175"/>
+        <img src={previewImage} alt={name} width="280" height="175" />
       </div>
       <h3 className="small-film-card__title">
         <Link className="small-film-card__link" to={`${AppRoute.FilmData}/${id}`}>{name}</Link>
